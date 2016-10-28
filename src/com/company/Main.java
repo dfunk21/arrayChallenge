29e4deed2d5d2d6b679af1645a5258879f8e9f14
dawsonfunk <dawsonfunk@gmail.com>
2016-10-28 00:12:56 -0400

    complete challenge 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,30 +7,44 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        Scanner scan1 = new Scanner(System.in);
         int counter = 0;
         int sum = 0;
+        int average = 0;
         int grades[] = new int[1000];
+        String nameList[] = new String[1000];
 
         while (true) {
-            System.out.println("User please enter grade");
-            int userInput = scan.nextInt();
+            System.out.print("User please enter student's name (type \"EXIT\" to print list of names and corresponding grades) ");
+            String userInputName = scan.nextLine();
 
-            if (userInput == -1)
+                nameList[counter] = userInputName;
+
+            if(userInputName.equalsIgnoreCase("exit"))
             {
                 break;
             }
-            else {
-                grades[counter] = userInput;
+
+
+            System.out.print("Please enter grade for student ");
+            int userInputGrade = scan1.nextInt();
+
+
+                grades[counter] = userInputGrade;
 
                 sum = sum + grades[counter];
                 counter++;
-            }
 
 
-
+            average = sum / counter;
         }
 
-        System.out.println("Average: " + (sum/counter));
+        for (int i = 0; i<counter; i++)
+        {
+            System.out.println(nameList[i] + " : " + grades[i]);
+        }
+
+        System.out.println("Average: " + average);
 
         //add the ability to enter the name and grade
         //keep the two arrays parallel so that grades[0] and names[0] correlate to same person
